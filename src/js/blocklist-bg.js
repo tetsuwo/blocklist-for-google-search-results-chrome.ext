6169a3e20724715801b9f06e45d830c5af6fd720
tetsuwo <tetsuwo.oishi@gmail.com>
2015-08-08 12:49:30 +0900

    Rename js file

--- src/js/blocklist-bg.js
+++ src/js/blocklist-bg.js
@@ -6,71 +6,72 @@
 
 Blocklist.bg = {};
 
-Blocklist.bg.listenTabs = function() {
-    chrome.tabs.onActivated.addListener(function(info) {
-        chrome.tabs.get(info.tabId, function(tab) {
-            //console.log('onActivated', tab);
-            if (Blocklist.common.matchInjectionList(tab.url)) {
-                Blocklist.bg.runBlock();
+(function(bg) {
+
+    bg.listenTabs = function() {
+        chrome.tabs.onActivated.addListener(function(info) {
+            chrome.tabs.get(info.tabId, function(tab) {
+                if (Blocklist.common.matchInjectionList(tab.url)) {
+                    bg.runBlock();
+                }
+            });
+        });
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+            if (changeInfo.status === 'loading') {
+                if (Blocklist.common.matchInjectionList(tab.url)) {
+                    bg.runBlock();
+                }
+                return;
             }
         });
-    });
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if (changeInfo.status === 'loading') {
-            //console.log('onUpdated', tab);
-            if (Blocklist.common.matchInjectionList(tab.url)) {
-                Blocklist.bg.runBlock();
+    };
+
+    bg.listenMessage = function() {
+        chrome.runtime.onMessage.addListener(function(request, sender, sendMessage) {
+            console.log('bg.runtime.onMessage', request);
+            switch (request.type) {
+                case Blocklist.sendType.GET_BLOCKLIST:
+                    sendMessage({
+                        type: Blocklist.sendType.SEND_BLOCKLIST,
+                        blocklist: db.get('regexp_blocklist')
+                    });
+                    break;
+
+                case Blocklist.sendType.GET_GSRP_MODE:
+                    sendMessage({
+                        type: Blocklist.sendType.SEND_GSRP_MODE,
+                        gsrpMode: db.get('gsrp_mode')
+                    });
+                    break;
+
+                default:
+                    break;
             }
-            return;
-        }
-    });
-};
+        });
+    };
 
-Blocklist.bg.listenMessage = function() {
-    chrome.runtime.onMessage.addListener(function(request, sender, sendMessage) {
-        console.log('bg.runtime.onMessage', request);
-        switch (request.type) {
-            case Blocklist.sendType.GET_BLOCKLIST:
-                sendMessage({
-                    type: Blocklist.sendType.SEND_BLOCKLIST,
-                    blocklist: db.get('regexp_blocklist')
-                });
-                break;
-
-            case Blocklist.sendType.GET_GSRP_MODE:
-                sendMessage({
-                    type: Blocklist.sendType.SEND_GSRP_MODE,
-                    gsrpMode: db.get('gsrp_mode')
-                });
-                break;
-
-            default:
-                break;
-        }
-    });
-};
-
-Blocklist.bg.getUrl = function(callback) {
-    chrome.tabs.getSelected(null, function(tab) {
-        if (tab.url) {
-            try {
-                chrome.tabs.executeScript(null, { file: 'js/injection-geturl.js' });
-            } catch (e) {
-                console.error('Blocklist.bg.getUrl', e);
+    bg.getUrl = function(callback) {
+        chrome.tabs.getSelected(null, function(tab) {
+            if (tab.url) {
+                try {
+                    chrome.tabs.executeScript(null, { file: 'js/injection-geturl.js' });
+                } catch (e) {
+                    console.error('bg.getUrl', e);
+                }
+            } else {
+                callback(null);
             }
-        } else {
-            callback(null);
-        }
-    });
-};
+        });
+    };
 
-Blocklist.bg.runBlock = function() {
-    //console.log('runBlocklist');
-    try {
-        chrome.tabs.executeScript(null, { file: 'js/common.js' });
-        chrome.tabs.executeScript(null, { file: 'js/blocklist-inject.js' });
-        chrome.tabs.executeScript(null, { file: 'js/injection-run.js' });
-    } catch (e) {
-        console.error('Blocklist.bg.runBlock', e);
-    }
-};
+    bg.runBlock = function() {
+        try {
+            chrome.tabs.executeScript(null, { file: 'js/blocklist-common.js' });
+            chrome.tabs.executeScript(null, { file: 'js/blocklist-inject.js' });
+            chrome.tabs.executeScript(null, { file: 'js/injection-run.js' });
+        } catch (e) {
+            console.error('bg.runBlock', e);
+        }
+    };
+
+})(Blocklist.bg);
