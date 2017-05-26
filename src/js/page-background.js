d33365c55d13774c1c98cd861b401544e3cf99fa
tetsuwo <tetsuwo.oishi@gmail.com>
2017-05-26 23:22:11 +0900

    Adding context menu

--- src/js/page-background.js
+++ src/js/page-background.js
@@ -6,3 +6,15 @@
 
 Blocklist.bg.listenTabs();
 Blocklist.bg.listenMessage();
+
+
+chrome.contextMenus.create({
+  "title" : getMessage('add_domain_with_context_menu'),
+  "type"  : "normal",
+  "contexts" : ["all"],
+  "onclick" : function(info) {
+    console.log('info', info);
+    var url = "https://www.google.co.jp/"
+    chrome.tabs.create({ url : url});
+  }
+});
